a1fbd79d266b3b3f23a30e4a527c8192d5e9a450
luruibo <luruibo1995@163.com>
2017-10-06 17:05:10 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/luruibo/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/luruibo/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.luruibo.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.luruibo.coolweather.gson.Forecast;
 import com.example.luruibo.coolweather.gson.Weather;
+import com.example.luruibo.coolweather.service.AutoUpdateService;
 import com.example.luruibo.coolweather.util.HttpUtil;
 import com.example.luruibo.coolweather.util.Utility;
 
@@ -214,5 +216,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
